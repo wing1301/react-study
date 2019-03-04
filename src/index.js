442ebf9419e99688262b8148713cc221455b2ebd
wing1301 <240478108@qq.com>
2019-03-04 22:13:54 +0800

    zujian

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import ReactDOM from 'react-dom'
 // const mydiv = React.createElement('div',{id:'mydiv',title:'divvv'},'这是一个div元素')
 //在js文件中不识别以下这种类似html的语法，为了解决这个问题可以安装bable来进行转换
 // 在js中写入类似html的语法是jsx语法
-const mydiv = <div id="mydiv" title="divvv">这是一个div元素</div>
+function Welcome(props){
+    return <h1>hello,{props.name}</h1>
+}
 
-ReactDOM.render(mydiv,document.getElementById('app'))
+function APP(){
+    return (
+        <div>
+            <Welcome name="sasra"></Welcome>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <APP/>,
+    document.getElementById("app")
+)
