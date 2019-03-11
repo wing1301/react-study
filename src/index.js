47efdfd8d27a8a2ed926f75476c1cf0af456e4b1
wing1301 <240478108@qq.com>
2019-03-11 21:18:03 +0800

    state

--- src/index.js
+++ src/index.js
@@ -5,19 +5,49 @@ import ReactDOM from 'react-dom'
 // const mydiv = React.createElement('div',{id:'mydiv',title:'divvv'},'这是一个div元素')
 //在js文件中不识别以下这种类似html的语法，为了解决这个问题可以安装bable来进行转换
 // 在js中写入类似html的语法是jsx语法
-function Welcome(props){
-    return <h1>hello,{props.name}</h1>
+
+
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date:new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello,World</h1>
+                <p>It is {this.state.date.toLocaleTimeString()}.</p>
+                <p>It is {this.state.date.toLocaleTimeString()}.</p>
+                <p>It is {this.state.date.toLocaleTimeString()}.</p>
+            </div>
+        )
+    }
 }
 
-function APP(){
-    return (
-        <div>
-            <Welcome name="sasra"></Welcome>
-        </div>
-    )
-}
 
 ReactDOM.render(
-    <APP/>,
+    <Clock />,
     document.getElementById("app")
-)
+);
+
+// setInterval(tick,1000);
+
+
